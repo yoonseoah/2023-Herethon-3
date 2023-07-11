40b4ca56c92297791b5277ecd00c19df843d5b49
Seonah Lee <126681896+3eonah@users.noreply.github.com>
2023-07-12 01:04:31 +0900

    Delete js directory

--- js/world.js
+++
@@ -1,6 +0,0 @@
-window.initMap = function () {
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 37.5400456, lng: 126.9921017 },
-        zoom: 1,
-    });
-};
